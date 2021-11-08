19930cb53a84b9bf6e442864c370439711c33985
Abdulmajeed Alduaifi <438101329@student.ksu.edu.sa>
2021-11-08 11:58:57 +0300

    some changes

--- my_first_express_app/server.js
+++ my_first_express_app/server.js
@@ -3,20 +3,20 @@ const app = express();
 const fs = require("fs");
 
 app.get("/", function (req, res) {
-  fs.readFile("./person.json", "utf-8", (er, data) => {
-    let d = JSON.parse(data);
-    res.send("Welcome " + d.name);
+  fs.readFile("./public/person.json", "utf-8", (er, data) => {
+    let obj = JSON.parse(data);
+    res.send("Welcome " + obj.name);
   });
 });
 app.get("/about.html", function (req, res) {
-  fs.readFile("./about.html", (err, data) => {
+  fs.readFile("./public/about.html", (err, data) => {
     if (err) console.log(err);
     res.write(data);
     return res.end();
   });
 });
 app.get("/contact_us.html", function (req, res) {
-  fs.readFile("./contact_us.html", (err, data) => {
+  fs.readFile("./public/contact_us.html", (err, data) => {
     if (err) console.log(err);
     res.write(data);
     return res.end();
