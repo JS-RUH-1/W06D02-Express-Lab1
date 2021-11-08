17b26a700253b64d21a74420131c95e0886b19e6
Abdulmajeed Alduaifi <438101329@student.ksu.edu.sa>
2021-11-08 11:54:38 +0300

    lab is done

---
+++ my_first_express_app/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", function (req, res) {
+  fs.readFile("./person.json", "utf-8", (er, data) => {
+    let d = JSON.parse(data);
+    res.send("Welcome " + d.name);
+  });
+});
+app.get("/about.html", function (req, res) {
+  fs.readFile("./about.html", (err, data) => {
+    if (err) console.log(err);
+    res.write(data);
+    return res.end();
+  });
+});
+app.get("/contact_us.html", function (req, res) {
+  fs.readFile("./contact_us.html", (err, data) => {
+    if (err) console.log(err);
+    res.write(data);
+    return res.end();
+  });
+});
+app.get("/*", function (req, res) {
+  fs.readFile("./contact_us.html", (err, data) => {
+    res.send("Sorry! Can’t find that resource. Please check your URL");
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
